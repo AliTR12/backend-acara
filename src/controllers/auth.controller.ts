a3ab17eb8f823ebddadcdaa409ffd6e2a0769697
AliTR12 <alitawfiqurrahman@gmail.com>
2025-03-14 21:14:29 +0700

    feat: deploy user activation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,7 +22,19 @@ const registerValidateSchema = Yup.object().shape({
   fullName: Yup.string().required(),
   username: Yup.string().required(),
   email: Yup.string().email().required(),
-  password: Yup.string().required(),
+  password: Yup.string()
+    .required()
+    .min(6, "Password must be at least 6 characters")
+    .test("at-least-one-uppercase-letter", "Contains at least one uppercase letter", (value) => {
+      if (!value) return false;
+      const regex = /^(?=.*[A-Z])/;
+      return regex.test(value);
+    })
+    .test("at-least-one-number", "Contains at least one number", (value) => {
+      if (!value) return false;
+      const regex = /^(?=.*\d)/;
+      return regex.test(value);
+    }),
   confirmPassword: Yup.string()
     .required()
     .oneOf([Yup.ref("password")], "Password must match"),
@@ -30,7 +42,22 @@ const registerValidateSchema = Yup.object().shape({
 
 export default {
   async register(req: Request, res: Response) {
-    const { fullName, username, email, password, confirmPassword } = req.body as unknown as TRegister;
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: '#/components/schemas/RegisterRequest'},
+     }
+     */
+    const { fullName, username, email, password, confirmPassword } = req.body as TRegister;
+
+    if (!username) {
+      return res.status(400).json({
+        message: "Username is required",
+        data: null,
+      });
+    }
+
     try {
       await registerValidateSchema.validate({
         fullName,
@@ -50,35 +77,44 @@ export default {
         data: result,
       });
     } catch (error) {
-      const err = error as unknown as Error;
+      const err = error as Error;
       res.status(400).json({
         message: err.message,
         data: null,
       });
     }
   },
+
   async login(req: Request, res: Response) {
     /**
-     #swager.requestBody = {
-     required: true,
-     schema: {
-       $ref: "#/components/schemas/LoginRequest",
-       }
-       }
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: '#/components/schemas/LoginRequest'},
+     }
      */
-    //ambil data user berdasarkan "identifier" -> username atau email
-    const { identifier, password } = req.body as unknown as TLogin;
+    const { identifier, password } = req.body as TLogin;
+
+    // if (!identifier) {
+    //   return res.status(400).json({
+    //     message: "Identifier (username or email) is required",
+    //     data: null,
+    //   });
+    // }
+
     try {
       const userByIdentifier = await UserModel.findOne({
         $or: [{ username: identifier }, { email: identifier }],
+        isActive: true,
       });
+
       if (!userByIdentifier) {
         return res.status(403).json({
           message: "User not found",
           data: null,
         });
       }
-      //validasi password
+
       const validatePassword: boolean = encrypt(password) === userByIdentifier.password;
       if (!validatePassword) {
         return res.status(403).json({
@@ -86,6 +122,7 @@ export default {
           data: null,
         });
       }
+
       const token = generateToken({
         id: userByIdentifier._id,
         role: userByIdentifier.role,
@@ -96,17 +133,19 @@ export default {
         data: token,
       });
     } catch (error) {
-      const err = error as unknown as Error;
+      const err = error as Error;
       res.status(400).json({
         message: err.message,
         data: null,
       });
     }
   },
+
   async me(req: IReqUser, res: Response) {
     /**
+     #swagger.tags = ['Auth']
      #swagger.security = [{
-      "bearerAuth": []
+     "bearerAuth": []
      }]
      */
     try {
@@ -117,7 +156,30 @@ export default {
         data: result,
       });
     } catch (error) {
-      const err = error as unknown as Error;
+      const err = error as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+  async activation(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: '#/components/schemas/ActivationRequest'},
+     }
+     */
+    try {
+      const { code } = req.body as { code: string };
+      const user = await UserModel.findOneAndUpdate({ activationCode: code }, { isActive: true }, { new: true });
+      res.status(200).json({
+        message: "User successfully activated",
+        data: user,
+      });
+    } catch (error) {
+      const err = error as Error;
       res.status(400).json({
         message: err.message,
         data: null,
--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -2,37 +2,47 @@ import { version } from "mongoose";
 import swaggerAutogen from "swagger-autogen";
 
 const doc = {
-    info: {
-        version: "v0.0.1",
-        title: "Dokumentasi API Acara",
-        description: "Dokumentasi API Acara",
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API Acara",
+    description: "Dokumentasi API Acara",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
     },
-    servers: [
-        {
-            url: "http://localhost:3000/api",
-            description: "Local Server",
-        },
-        {
-            url: "https://backend-acara-sigma.vercel.app/api",
-            description: "Deploy Server",
-        }
-    ],
-    components: {
-        securitySchemes: {
-            bearerAuth: {
-                type: "http",
-                scheme: "bearer",
-            },
-        },
-        schemas: {
-            LoginRequest: {
-                identifier: "alitr",
-                password: "Atr12",
-            }
-        }
-    }
-}
+    {
+      url: "https://backend-acara-sigma.vercel.app/api",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "alitr",
+        password: "Atr12",
+      },
+      RegisterRequest: {
+        fullName: "Ucup",
+        username: "ucup",
+        email: "fc861d5y1587@student.devacademy.id",
+        password: "1234",
+        confirmPassword: "1234",
+      },
+      ActivationRequest: {
+        code: "abcdef"
+      }
+    },
+  },
+};
 const outputFile = "./swagger_output.json";
 const endpointsFiles = ["../routes/api.ts"];
 
-swaggerAutogen({openapi: "3.0.0"})(outputFile, endpointsFiles, doc);
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
